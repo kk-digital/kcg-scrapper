8f31743d3328c6ada45bfe35d86acda6c2ff36c3
Leiver Moreno <leivermoreno@gmail.com>
2023-04-29 16:31:50 -0400

    save collected data into jsonl

--- pinterest-scrapper/pinterest_scraper/download_stage.py
+++ pinterest-scrapper/pinterest_scraper/download_stage.py
@@ -1,23 +1,30 @@
 import logging
 import os
+import time
 import uuid
 from os import path
 from sqlite3 import Row
-from typing import List
+from typing import List, Optional
 from urllib.parse import urlparse, urlunparse
 
+import jsonlines
 from requests import Session, RequestException, Response
 from selenium.common import TimeoutException
 
 from pinterest_scraper.stage import Stage
-from settings import MAX_RETRY, OUTPUT_FOlDER, TIMEOUT
+from settings import MAX_RETRY, OUTPUT_FOlDER, TIMEOUT, DOWNLOAD_DELAY
 
 logger = logging.getLogger(f"scraper.{__name__}")
 
 
 class DownloadStage(Stage):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__json_fh: Optional[jsonlines.Writer] = None
+
     def __init_output_dir(self) -> None:
         dir_path = path.join(OUTPUT_FOlDER, "jobs", self._job["query"])
+        self.__output_path = dir_path
         self.__images_path = path.join(dir_path, "images")
         self.__html_path = path.join(dir_path, "html")
         os.makedirs(self.__images_path, exist_ok=True)
@@ -49,7 +56,7 @@ class DownloadStage(Stage):
 
         return new_urls
 
-    def __save_img(self, res: Response, img_url: str, pin_uuid: uuid.UUID) -> None:
+    def __save_img(self, res: Response, img_url: str, pin_uuid: uuid.UUID) -> str:
         ext = path.splitext(img_url)[1]
         basename = f"{pin_uuid}{ext}"
         img_path = path.join(self.__images_path, basename)
@@ -57,9 +64,11 @@ class DownloadStage(Stage):
         with open(img_path, "wb") as fh:
             fh.write(res.content)
 
+        return basename
+
     def __download_pin_img(
             self, session: Session, pin: Row, pin_uuid: uuid.UUID
-    ) -> None:
+    ) -> str:
         img_urls = self.__get_img_urls(pin["img_url"])
         for img_url in img_urls:
             res = session.get(img_url, timeout=TIMEOUT)
@@ -70,16 +79,33 @@ class DownloadStage(Stage):
 
             res.raise_for_status()
 
-            self.__save_img(res, img_url, pin_uuid)
+            img_name = self.__save_img(res, img_url, pin_uuid)
 
             break
 
-    def __save_pin_html(self, session: Session, pin: Row, pin_uuid: uuid.UUID) -> None:
+        # noinspection PyUnboundLocalVariable
+        return img_name
+
+    def __save_pin_html(self, pin: Row, pin_uuid: uuid.UUID) -> None:
         self._driver.get(pin["url"])
 
         file_path = path.join(self.__html_path, f"{pin_uuid}.html")
         with open(file_path, "w", encoding="utf-8") as fh:
             fh.write(self._driver.page_source)
+
+    def __add_to_json(self, pin_uuid: uuid.UUID, img_name: str) -> None:
+        if not self.__json_fh:
+            self.__json_fh = jsonlines.open(
+                path.join(self.__output_path, "pins.jsonl"), "a"
+            )
+
+        self.__json_fh.write(
+            dict(
+                query=self._job["query"],
+                img_name=img_name,
+                html_name=f"{pin_uuid}.html",
+            )
+        )
 
     def start_scraping(self) -> None:
         super().start_scraping()
@@ -96,16 +122,19 @@ class DownloadStage(Stage):
                     )
                     for pin in pins:
                         pin_uuid = uuid.uuid1()
-                        self.__download_pin_img(session, pin, pin_uuid)
-                        self.__save_pin_html(session, pin, pin_uuid)
+                        img_name = self.__download_pin_img(session, pin, pin_uuid)
+                        self.__save_pin_html(pin, pin_uuid)
+                        self.__add_to_json(pin_uuid, img_name)
 
                         self._db.update_board_or_pin_done_by_url("pin", pin["url"], 1)
                         retries = 0
                         logger.info(f"Successfully scraped pin {pin['url']}.")
+                        time.sleep(DOWNLOAD_DELAY)
 
                     break
                 except (RequestException, TimeoutException):
                     if retries == MAX_RETRY:
+                        self.__json_fh.close()
                         raise
 
                     logger.exception(
