dd5e060054b09a3ad422cb43a51243c5a1fe2603
Leiver Moreno <leivermoreno@gmail.com>
2023-04-30 13:17:57 -0400

    documented settings

--- pinterest-scrapper/settings.py
+++ pinterest-scrapper/settings.py
@@ -1,15 +1,27 @@
 # logging level, e.g. if set to warning no debug and info logs are sent to console nor log files
 LOG_LEVEL = "DEBUG"
 
-# where files and logs will be saved
+# path to output folder, relative and absolute allowed
+# can also set it with --output flag on cli
 OUTPUT_FOlDER = "output"
 
+# name of sqlite db the app will create to store persistent state of jobs
 DATABASE_NAME = "pinterest.sqlite"
 
+# delay between scrolls when scraping boards and pins
 SCROLL_DELAY = 0.25
+# delay between requests to pin page and pin img
 DOWNLOAD_DELAY = 2
 
-TIMEOUT = 30
+# max time an operation such as page loading or finding an element on dom
+# waits before raising an exception
+TIMEOUT = 15
 
+# number of retries in case of exception such as Timeout and NoSuchElement
 MAX_RETRY = 2
-MAX_OUTPUT_SIZE = 500000000  # bytes
+# max size before compressing
+MAX_OUTPUT_SIZE_MB = 500  # MB
+
+# DO NOT MODIFY
+# max size in bytes
+MAX_OUTPUT_SIZE = MAX_OUTPUT_SIZE_MB * (1024 * 1024)  # bytes
