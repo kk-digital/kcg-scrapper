0fbe2a6c280727ed9d0de1e5145dc5c5006fefec
leivermoreno <leivermoreno@gmail.com>
2023-05-22 17:50:47 -0400

    Revert "changed thread safety to 3 sqlite"
    This reverts commit 796538761714551044222303dd49a7a9379405fa.

--- pinterest-scrapper/src/db.py
+++ pinterest-scrapper/src/db.py
@@ -7,7 +7,6 @@ from typing import List, Optional, Callable
 
 import settings
 
-sqlite3.threadsafety = 3
 logger = logging.getLogger(f"scraper.{__name__}")
 _conn: Optional[sqlite3.Connection] = None
 lock = threading.Lock()
