262d84e43172a0a2310f062f4b1d64e3df5dc0f4
Leiver Moreno <leivermoreno@gmail.com>
2023-07-02 16:19:21 -0400

    added .env

--- midjourney-scraper/command.py
+++ midjourney-scraper/command.py
@@ -1,6 +1,13 @@
+import os
+
+if os.getenv("PYTHON_ENV", "development") == "development":
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
 from src import logging_
-from src.scraper import Scraper
+from src.scraper import start_scraping
 
 logging_.configure()
 
-Scraper().start_scraping()
+start_scraping()
--- midjourney-scraper/settings.py
+++ midjourney-scraper/settings.py
@@ -1,4 +1,9 @@
 import logging
+import os
 
-OUTPUT_FOLDER = "/output"
-LOGGING_LEVEL = logging.DEBUG
+is_production = True if os.getenv("PYTHON_ENV") == "production" else False
+
+OUTPUT_FOLDER = os.environ["OUTPUT_FOLDER"]
+LOGGING_LEVEL = logging.INFO if is_production else logging.DEBUG
+HEADED = not is_production
+ACTIONS_DELAY = 100  # milliseconds
