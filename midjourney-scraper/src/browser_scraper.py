0e808d6ed52db459590fe4568d027023da2b7c48
Leiver Moreno <leivermoreno@gmail.com>
2023-07-09 13:32:46 -0400

    removed set ua to context due to poor performance

--- midjourney-scraper/src/browser_scraper.py
+++ midjourney-scraper/src/browser_scraper.py
@@ -1,5 +1,4 @@
 import csv
-import json
 import logging
 import random
 from typing import Optional, List
@@ -19,25 +18,19 @@ class BrowserScraper:
     def __init__(self) -> None:
         self._logger = logging.getLogger(f"scraper.{__name__}")
         self._headless = not settings.HEADED
-        self._user_agent_list = settings.USER_AGENT_LIST
         self._viewport_sizes = settings.VIEWPORT_SIZES
         self._proxy_list_path = settings.PROXY_LIST
-        self._ua_list_path = settings.USER_AGENT_LIST
-        self._proxy_list: Optional[List[dict]] = None
-        self._ua_list: Optional[List[dict]] = None
+        self._proxy_list = self._load_proxy_list()
         self._playwright: Optional[Playwright] = None
         self._browser: Optional[Browser] = None
         self._context: Optional[BrowserContext] = None
-        self._load_lists()
 
-    def _load_lists(self) -> None:
-        if self._proxy_list_path:
-            with open(self._proxy_list_path, "r", encoding="utf-8", newline="") as fp:
-                self._proxy_list = list(csv.DictReader(fp))
+    def _load_proxy_list(self) -> Optional[List[dict]]:
+        if not self._proxy_list_path:
+            return
 
-        if self._ua_list_path:
-            with open(self._ua_list_path, "r", encoding="utf-8") as fp:
-                self._ua_list = json.load(fp)
+        with open(self._proxy_list_path, "r", encoding="utf-8", newline="") as fp:
+            return list(csv.DictReader(fp))
 
     def start_scraping(self) -> None:
         self._logger.info("Starting browser.")
@@ -45,16 +38,13 @@ class BrowserScraper:
         self._browser = self._playwright.firefox.launch(headless=self._headless)
 
     def init_context(self, base_url: Optional[str]) -> None:
-        ua = random.choice(self._ua_list)["ua"]
         width, height = random.choice(self._viewport_sizes)
         viewport = {"width": width, "height": height}
         proxy = random.choice(self._proxy_list) if self._proxy_list_path else None
-        self._logger.info(
-            f"Configuration: UA: {ua}, viewport: {viewport}, proxy: {proxy}."
-        )
+        self._logger.info(f"Configuration: viewport: {viewport}, proxy: {proxy}.")
 
         self._context = self._browser.new_context(
-            base_url=base_url, user_agent=ua, viewport=viewport, proxy=proxy
+            base_url=base_url, viewport=viewport, proxy=proxy
         )
         self._context.add_init_script(
             "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})"
