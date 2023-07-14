d68cfd692ecd5f2de996ef7a1ca0d6bb4e0fe354
Leiver Moreno <leivermoreno@gmail.com>
2023-07-14 11:39:45 -0400

    refactor how to get proxy from list

--- midjourney-scraper/src/browser_scraper.py
+++ midjourney-scraper/src/browser_scraper.py
@@ -1,7 +1,7 @@
 import csv
 import logging
 import random
-from typing import Optional, List
+from typing import Optional
 
 from playwright.sync_api import (
     sync_playwright,
@@ -20,27 +20,36 @@ class BrowserScraper:
         self._headless = not settings.HEADED
         self._viewport_sizes = settings.VIEWPORT_SIZES
         self._proxy_list_path = settings.PROXY_LIST
-        self._proxy_list = self._load_proxy_list()
         self._playwright: Optional[Playwright] = None
         self._browser: Optional[Browser] = None
         self._context: Optional[BrowserContext] = None
-
-    def _load_proxy_list(self) -> Optional[List[dict]]:
-        if not self._proxy_list_path:
-            return
-
-        with open(self._proxy_list_path, "r", encoding="utf-8", newline="") as fp:
-            return list(csv.DictReader(fp))
 
     def start_scraping(self) -> None:
         self._logger.info("Starting browser.")
         self._playwright = sync_playwright().start()
         self._browser = self._playwright.firefox.launch(headless=self._headless)
 
+    def _get_proxy(self) -> Optional[dict]:
+        if not self._proxy_list_path:
+            return
+
+        with open(self._proxy_list_path, "r", newline="", encoding="utf-8") as fp:
+            proxy_list = list(csv.DictReader(fp))
+
+        proxy = proxy_list.pop(0)
+        proxy_list.append(proxy)
+
+        with open(self._proxy_list_path, "w", newline="", encoding="utf-8") as fp:
+            writer = csv.DictWriter(fp, fieldnames=proxy.keys())
+            writer.writeheader()
+            writer.writerows(proxy_list)
+
+        return proxy
+
     def init_context(self, base_url: Optional[str]) -> None:
         width, height = random.choice(self._viewport_sizes)
         viewport = {"width": width, "height": height}
-        proxy = random.choice(self._proxy_list) if self._proxy_list_path else None
+        proxy = self._get_proxy()
         self._logger.info(f"Configuration: viewport: {viewport}, proxy: {proxy}.")
 
         self._context = self._browser.new_context(
