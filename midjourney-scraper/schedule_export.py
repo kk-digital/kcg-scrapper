002b3698d5d1b7bdaf79e5c106cc4fe66133153c
Leiver Moreno <leivermoreno@gmail.com>
2023-08-07 01:28:22 -0400

    added move zips to general weekly folder

--- midjourney-scraper/schedule_export.py
+++ midjourney-scraper/schedule_export.py
@@ -12,35 +12,39 @@ if os.getenv("PYTHON_ENV", "development") == "development":
 
     load_dotenv()
 
-
 import settings
 from src.utils import Utils
-
 
 utils = Utils()
 filters_path = sys.argv[1]
 
 
 def job():
+    weekly_folder = Path(settings.OUTPUT_FOLDER, f"output-{date.today().isoformat()}")
+    weekly_folder.mkdir()
+
     prompt_filters = utils.read_filters(filters_path)
     for prompt_filter in prompt_filters:
         utils.export_json_data(prompt_filter=prompt_filter, test_export=False)
         utils.compress_output(test_export=False)
 
-        weekly_folder = Path(
+        filter_weekly_folder = Path(
             settings.OUTPUT_FOLDER, f"{prompt_filter}-{date.today().isoformat()}"
         )
-        weekly_folder.mkdir()
+        filter_weekly_folder.mkdir()
 
-        shutil.move(settings.EXPORT_JSON_PATH, weekly_folder)
+        shutil.move(settings.EXPORT_JSON_PATH, filter_weekly_folder)
         zip_list = Path(settings.OUTPUT_FOLDER).glob("*.zip")
         for zipfile in zip_list:
-            shutil.move(zipfile, weekly_folder)
+            shutil.move(zipfile, filter_weekly_folder)
 
         shutil.make_archive(
-            base_name=str(weekly_folder), format="zip", root_dir=weekly_folder
+            base_name=str(filter_weekly_folder),
+            format="zip",
+            root_dir=filter_weekly_folder,
         )
-        shutil.rmtree(weekly_folder)
+        shutil.move(filter_weekly_folder.with_suffix(".zip"), weekly_folder)
+        shutil.rmtree(filter_weekly_folder)
 
 
 schedule.every().friday.do(job)
