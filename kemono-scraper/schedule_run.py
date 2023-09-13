7487c65328fbe125e6f2c647aa977ce503f2143f
Leiver Moreno <leivermoreno@gmail.com>
2023-09-13 09:04:31 -0400

    Revert "run job in separate thread"
    This reverts commit b6278c24ddb7745a7834f0dedb63c005b37515db.

--- kemono-scraper/schedule_run.py
+++ kemono-scraper/schedule_run.py
@@ -2,7 +2,6 @@ import shutil
 import time
 from datetime import date
 from pathlib import Path
-from threading import Thread
 
 import schedule
 from command import Command
@@ -36,11 +35,6 @@ def compress_output_every_friday(settings, jsonl_path):
     shutil.rmtree(weekly_folder)
 
 
-def run_threaded(job_func):
-    job_thread = Thread(target=job_func)
-    job_thread.start()
-
-
 def main(jsonl_path: str, job_dir: str | None = None, run_now: bool = False):
     settings = get_project_settings()
     settings["FEEDS"] = {
@@ -54,10 +48,8 @@ def main(jsonl_path: str, job_dir: str | None = None, run_now: bool = False):
     if run_now:
         compress_output_every_friday(settings, jsonl_path)
 
-    schedule.every().day.at("12:00").do(run_threaded, lambda: daily_run(settings))
-    schedule.every().saturday.do(
-        run_threaded, lambda: compress_output_every_friday(settings, jsonl_path)
-    )
+    schedule.every().day.at("12:00").do(daily_run, settings)
+    schedule.every().saturday.do(compress_output_every_friday, settings, jsonl_path)
 
     while True:
         schedule.run_pending()
