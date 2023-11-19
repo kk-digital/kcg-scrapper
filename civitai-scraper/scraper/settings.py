d4d21472c1399d1c142db2d2c3a1b17cd492d5a8
Leiver Moreno <leivermoreno@gmail.com>
2023-11-19 18:12:13 -0400

    disable obey robots

--- civitai-scraper/scraper/settings.py
+++ civitai-scraper/scraper/settings.py
@@ -21,7 +21,7 @@ IMAGES_STORE = OUTPUT_DIR / "images"
 # USER_AGENT = "scraper (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 32
