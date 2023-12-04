18e97bd572f46ed13cf7cd406d46234469ca68c1
Leiver Moreno <leivermoreno@gmail.com>
2023-12-03 20:54:18 -0400

    add pin stage

--- pinterest-scraper/scraper/spiders/pins.py
+++ pinterest-scraper/scraper/spiders/pins.py
@@ -3,9 +3,10 @@ from typing import Iterable
 
 import scrapy
 from playwright.async_api import Page
-from scrapy.http import Request
+from scrapy.http import Request, Response
 
 from scraper.views.board_grid import BoardGridView
+from scraper.views.pin_grid import PinGridView
 
 
 class PinsSpider(scrapy.Spider):
@@ -41,14 +42,34 @@ class PinsSpider(scrapy.Spider):
         page = failure.request.meta["playwright_page"]
         await page.close()
 
-    async def extract_board_urls(self, response):
+    async def extract_board_urls(self, response: Response):
         page: Page = response.meta["playwright_page"]
-        await page.wait_for_timeout(5000)
+        await page.wait_for_timeout(5000)  # TODO move to view
 
         view = BoardGridView(page, self.settings)
         await view.start_view()
         board_urls = view.get_board_urls()
-        print(board_urls)
-        print(len(board_urls))
-
         await page.close()
+
+        for url in board_urls:
+            yield response.follow(
+                url,
+                callback=self.extract_pin_urls,
+                meta={
+                    "playwright": True,
+                    "playwright_include_page": True,
+                    "playwright_page_init_callback": self.init_page,
+                },
+                errback=self.errback_close_page,
+            )
+
+    async def extract_pin_urls(self, response: Response):
+        page: Page = response.meta["playwright_page"]
+        await page.wait_for_timeout(5000)
+
+        view = PinGridView(page, self.settings)
+        await view.start_view()
+        pin_urls = view.get_pin_urls()
+        await page.close()
+
+        print(pin_urls)
---
+++ pinterest-scraper/scraper/views/pin_grid.py
@@ -0,0 +1,34 @@
+import re
+import urllib.parse
+
+from bs4 import BeautifulSoup
+from playwright.async_api import Page
+from scrapy.settings import Settings
+
+from scraper.views import _utils
+
+
+class PinGridView:
+    def __init__(self, page: Page, settings: Settings) -> None:
+        self._page = page
+        self._pin_urls = set()
+        self._scroll_delay = settings["SCROLL_DELAY"]
+        self._check_bottom_times = settings["CHECK_BOTTOM_TIMES"]
+
+    async def _extract_urls(self):
+        html = await self._page.content()
+        soup = BeautifulSoup(html, "lxml")
+
+        for pin in soup.find_all("a", href=re.compile(r"\/pin\/\d+\/")):
+            self._pin_urls.add(urllib.parse.urljoin(self._page.url, pin["href"]))
+
+    async def start_view(self):
+        await _utils.scroll_to_bottom_while_do(
+            page=self._page,
+            scroll_delay=self._scroll_delay,
+            check_bottom_times=self._check_bottom_times,
+            do=self._extract_urls,
+        )
+
+    def get_pin_urls(self):
+        return self._pin_urls
