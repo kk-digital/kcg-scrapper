9476fca3676b689e32e50cd73ed5c716d83040b5
Leiver Moreno <leivermoreno@gmail.com>
2023-12-08 20:06:49 -0400

    replace ua middleware with custom for playwright

--- pinterest-scraper/scraper/settings.py
+++ pinterest-scraper/scraper/settings.py
@@ -79,6 +79,8 @@ DOWNLOAD_DELAY = 1
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
+    "scrapy.downloadermiddlewares.useragent.UserAgentMiddleware": None,
+    "scraper.middlewares.CustomUserAgentMiddleware": 500,
     "rotating_proxies.middlewares.RotatingProxyMiddleware": 610,
     "rotating_proxies.middlewares.BanDetectionMiddleware": 620,
 }
