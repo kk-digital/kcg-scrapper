9212b9b0ad6989dee38d2eb03bc6eb7a5b613ab9
Leiver Moreno <leivermoreno@gmail.com>
2024-02-10 12:22:33 -0400

    Add new files and update requirements.txt

--- pinterest-scraper-playwright/src/scraper.py
+++ pinterest-scraper-playwright/src/scraper.py
@@ -1,3 +1,58 @@
+import itertools
+import urllib.parse
+from typing import Iterator
+
+from sqlalchemy import Engine
+from sqlalchemy.orm import Session
+
+from src.browser import Browser
+from src.db import setup_db
+from src.settings import OUTPUT_DIR, PROXY_LIST_PATH
+from src.views.board_grid import BoardGridView
+
+
 class Scraper:
     def __init__(self, query: str) -> None:
-        self.query = query
+        query = urllib.parse.quote_plus(query)
+        self.initial_url = (
+            f"https://www.pinterest.com/search/boards/?q={query}&rs=typed"
+        )
+        self.engine: Engine | None = None
+        self.session: Session | None = None
+        self.proxy_list: Iterator | None = None
+
+    def load_proxy_list(self) -> list[dict]:
+        proxy_list = []
+        with PROXY_LIST_PATH.open("r", encoding="utf-8") as fp:
+            for line in fp:
+                [credentials, server] = line.split("@")
+                [username, password] = credentials.split(":")
+                proxy_list.append(
+                    {"server": server, "username": username, "password": password}
+                )
+
+        return proxy_list
+
+    def setup(self) -> None:
+        OUTPUT_DIR.mkdir(exist_ok=True)
+        self.engine = setup_db()
+        self.session = Session(self.engine)
+        self.proxy_list = itertools.cycle(self.load_proxy_list())
+
+    def scrape_board_urls(self) -> set:
+        with Browser(url=self.initial_url, proxy=next(self.proxy_list)) as page:
+            view = BoardGridView(page=page)
+            view.start_view()
+            urls = view.get_board_urls()
+
+        print(list(urls))
+
+        return urls
+
+    def run(self) -> None:
+        try:
+            self.setup()
+            self.scrape_board_urls()
+        finally:
+            if self.session is not None:
+                self.session.close()
--- pinterest-scraper-playwright/src/settings.py
+++ pinterest-scraper-playwright/src/settings.py
@@ -1,4 +1,12 @@
+import os
 from pathlib import Path
+
+is_production = os.getenv("PYTHON_ENV", "development") == "production"
 
 OUTPUT_DIR = Path("output")
 SQLITE_DB_PATH = OUTPUT_DIR / "db.sqlite3"
+PROXY_LIST_PATH = Path("proxies.txt")
+SCROLL_DELAY = 0.3
+CHECK_BOTTOM_TIMES = 33
+SHORT_WAIT = 3000
+HEADLESS = is_production
